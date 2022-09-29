3d1a9f086b2872e0d840990cdc09289530fbb4e1
bojianyin <2316744840@qq.com>
2022-09-29 19:13:12 +0800

    code

--- app/src/main/java/com/source/mymvvm/repo/MainReqManger.kt
+++ app/src/main/java/com/source/mymvvm/repo/MainReqManger.kt
@@ -1,17 +1,10 @@
 package com.source.mymvvm.repo
 
-import android.util.Log
 import com.source.mymvvm.repo.control.MyRetrofit
 import com.source.mymvvm.repo.services.ApiService
 import com.source.mymvvm.repo.services.ResResult
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.await
 
 class MainReqManger {
