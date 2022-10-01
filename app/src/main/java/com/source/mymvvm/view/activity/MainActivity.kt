02b9dd6fd885b09754e4c41534b767e114773821
薄健寅-FE <bojianyin@xiaolian.link>
2022-10-01 13:30:29 +0800

    xx

--- app/src/main/java/com/source/mymvvm/view/activity/MainActivity.kt
+++ app/src/main/java/com/source/mymvvm/view/activity/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.source.mymvvm.view.activity
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Intent
 import android.text.TextUtils
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.source.mymvvm.R
 import com.source.mymvvm.view.activity.base.BaseActivity
@@ -9,10 +13,12 @@ import com.source.mymvvm.databinding.ActivityMainBinding
 import com.source.mymvvm.model.EventContants
 import com.source.mymvvm.model.EventModel
 import com.source.mymvvm.utils.KeyBoardUtil
+import com.source.mymvvm.view.widget.SlideTabWidget
 import com.source.mymvvm.vm.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
 
+    private val mSlideTab:SlideTabWidget? by lazy { dataBinding?.mSlideTab }
     override fun getLayoutView(): Int = R.layout.activity_main
 
     override fun doInit() {
@@ -21,6 +27,81 @@ class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
         dataBinding?.vm = mViewModel
 
         dataBinding?.activity = this
+
+        mSlideTab?.apply {
+            add(
+                SlideTabWidget.TabEntry<String>(1).apply{
+                    text = "tab1"
+                    data = "hello"
+                }
+            )
+            add(
+                SlideTabWidget.TabEntry<String>(2).apply{
+                    text = "tab2"
+                    data = "hello2"
+                }
+            )
+            add(
+                SlideTabWidget.TabEntry<String>(3).apply{
+                    text = "tab3dasdsa"
+                    data = "hello3"
+                }
+            )
+            add(
+                SlideTabWidget.TabEntry<String>(4).apply{
+                    text = "tab4"
+                    data = "hello4"
+                }
+            )
+            add(
+                SlideTabWidget.TabEntry<String>(4).apply{
+                    text = "tab4"
+                    data = "hello4"
+                }
+            )
+            add(
+                SlideTabWidget.TabEntry<String>(4).apply{
+                    text = "tab4"
+                    data = "hello4"
+                }
+            )
+            add(
+                SlideTabWidget.TabEntry<String>(4).apply{
+                    text = "tab4"
+                    data = "hello4"
+                }
+            )
+            add(
+                SlideTabWidget.TabEntry<String>(4).apply{
+                    text = "tab4"
+                    data = "hello4"
+                }
+            )
+        }?.commit()
+
+
+        mSlideTab?.listener = object : SlideTabWidget.OnClickListener{
+            override fun onClick(position: Int, view: View) {
+                Log.d("Main","onClick"+position)
+                val mClipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+                if(mClipboardManager.hasPrimaryClip()){
+                    Toast.makeText(this@MainActivity,mClipboardManager.primaryClip?.getItemAt(0)?.text,Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this@MainActivity,"没有复制的内容",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            override fun reClick(position: Int, view: View) {
+                super.reClick(position, view)
+                Log.d("Main","reClick"+position)
+
+                val mClipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+                mClipboardManager.setPrimaryClip(ClipData.newPlainText("ceshi","ceshi"))
+
+            }
+        }
+
     }
 
     override fun getVMClass(): Class<MainViewModel> {
---
+++ app/src/main/java/com/source/mymvvm/view/widget/SlideTabWidget.kt
@@ -0,0 +1,139 @@
+package com.source.mymvvm.view.widget
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.core.view.marginLeft
+import com.source.mymvvm.R
+import kotlinx.android.synthetic.main.widget_slide_tab.view.*
+import kotlin.collections.ArrayList
+
+class SlideTabWidget @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : RelativeLayout(context, attrs, defStyleAttr) {
+
+    private var mlistData:ArrayList<TabEntry<*>> = arrayListOf()
+
+    private var tabLayout:LinearLayout? = null
+    //滑动线
+    private var line: View? = null
+
+    var listener:OnClickListener? = null
+
+    //现在选中的索引
+    var cPosition:Int? = null
+
+    private val lineAnimate:ValueAnimator by lazy {
+        ValueAnimator.ofInt(0,0)
+    }
+
+    init {
+        setBackgroundResource(R.drawable.bg_tab_radius)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        val layout = LayoutInflater.from(context).inflate(R.layout.widget_slide_tab,this,true)
+        line = layout.findViewById(R.id.line)
+
+        tabLayout = layout.findViewById(R.id.contentLayout)
+
+        lineAnimate?.addUpdateListener {
+            val layoutParams = LayoutParams(line?.layoutParams)
+            layoutParams.width = it.animatedValue as Int
+            layoutParams.addRule(ALIGN_PARENT_BOTTOM)
+            line?.layoutParams = layoutParams
+
+        }
+
+    }
+
+
+    fun <T> add(entry:TabEntry<T>):SlideTabWidget{
+        mlistData.add(entry)
+        return this
+    }
+
+    fun commit(){
+        cPosition = 0
+        for((index,entry) in mlistData.withIndex()){
+            val view = LayoutInflater.from(context).inflate(R.layout.widget_tab_item,this,false)
+
+            val title:TextView = view.findViewById(R.id.titleTab)
+            title.text = entry.text
+
+            view.setOnClickListener {
+                if(cPosition==null) {
+                    throw ExceptionInInitializerError("no commit!!!")
+                }
+                if(cPosition == index){
+                    listener?.reClick(cPosition!!,it)
+                    return@setOnClickListener
+                }
+                cPosition = index
+                listener?.onClick(cPosition!!,it)
+                moveView(it)
+            }
+
+            tabLayout?.addView(view)
+
+            if(index==0){
+                view?.post {
+                    moveView(view)
+                }
+
+            }
+        }
+
+    }
+
+    fun moveView(it:View){
+        val location = IntArray(2)
+        it.getLocationInWindow(location)
+
+        val pMargin = marginLeft
+
+
+        //改变位置x
+        line?.animate()?.translationX(location[0].toFloat()-pMargin+dptopx(12f)+scrollBar.scrollX)?.setDuration(300)?.start()
+
+        //改变宽度
+        lineAnimate.setIntValues(line!!.width,it.width-(dptopx(12f*2).toInt()))
+        lineAnimate.duration = 300
+        lineAnimate.start()
+
+
+        /*
+        * item距离控件的left距离 - 屏幕/2
+        * */
+
+        val pleft = it.left
+        val screenW = context.resources.displayMetrics.widthPixels
+        val v = pleft - screenW / 2 + it.width/2
+        scrollBar.smoothScrollTo(v,0)
+    }
+
+
+    fun dptopx(dp:Float):Float{
+        val density = context.resources.displayMetrics.density
+        return dp * density + 0.5f
+    }
+
+    data class TabEntry<T>(val tid:Int) {
+        var text:String? = null
+        var data:T? = null
+    }
+
+    interface OnClickListener{
+        fun onClick(position:Int,view:View)
+        fun reClick(position:Int,view:View){
+            Log.d("SlideTabWidget","reClick>>>")
+        }
+    }
+}
